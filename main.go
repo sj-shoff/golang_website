adcfa5d11ffc8b5a70624eabeda7b30b22c54e0b
sj_shoff <atryakhin0306#gmail.com>
2024-10-21 23:20:17 +0300

    modified:   .vscode/launch.json 	new file:   __debug_bin2585354476 	modified:   main.go

--- main.go
+++ main.go
@@ -12,8 +12,20 @@ type User struct {
 	avg_grades, happiness float64
 }
 
+func (u User) getAllInfo() string {
+	return fmt.Sprintf("User name is: %s. He is %d and he has money "+
+		"equal: %d", u.name, u.age, u.money)
+}
+
+func (u *User) setNewName(newName string) {
+	u.name = newName
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Go is super easy!")
+	serj := User{"Serj", 18, 0, 5.0, 0.5}
+	serj.setNewName("Sergey")
+	fmt.Fprintf(w, serj.getAllInfo())
+
 }
 
 func contacts_page(w http.ResponseWriter, r *http.Request) {
