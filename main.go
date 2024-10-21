0cfe182eeddfb36779b692ff468ca55213bec464
sj_shoff <atryakhin0306#gmail.com>
2024-10-21 22:40:18 +0300

    new file:   .vscode/launch.json 	modified:   go.mod 	modified:   main.go

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+type User struct {
+	name                  string
+	age                   uint16
+	money                 int16
+	avg_grades, happiness float64
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Go is super easy!")
+}
+
+func contacts_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Contacts page")
+}
+
+func handleRequest() {
+	http.HandleFunc("/", home_page)
+	http.HandleFunc("/contacts/", contacts_page)
+	http.ListenAndServe(":8080", nil)
+}
 func main() {
-	fmt.Println("Hi")
+	handleRequest()
 }
