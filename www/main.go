ddb13900bfb06a82da244454e9d4d82ddc030639
sj_shoff <atryakhin0306#gmail.com>
2024-10-21 23:47:12 +0300

    modified:   www/main.go 	new file:   www/templates/home_page.html

--- www/main.go
+++ www/main.go
@@ -2,29 +2,32 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
-	name                  string
-	age                   uint16
-	money                 int16
-	avg_grades, happiness float64
+	Name                  string
+	Age                   uint16
+	Money                 int16
+	Avg_grades, Happiness float64
 }
 
-func (u User) getAllInfo() string {
+func (u *User) getAllInfo() string {
 	return fmt.Sprintf("User name is: %s. He is %d and he has money "+
-		"equal: %d", u.name, u.age, u.money)
+		"equal: %d", u.Name, u.Age, u.Money)
 }
 
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
 	serj := User{"Serj", 18, 0, 5.0, 0.5}
-	serj.setNewName("Sergey")
-	fmt.Fprintf(w, serj.getAllInfo())
+	// serj.setNewName("Sergey")
+	// fmt.Fprintf(w, serj.getAllInfo())
+	tmpl, _ := template.ParseFiles("templates/home_page.html")
+	tmpl.Execute(w, serj)
 
 }
 
